230413ddacbce4a8df3b5cb63b0777f3ecf8815d
Ankur Israni <ankur.israni@aa.com>
2020-02-23 11:36:28 -0600

    all done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'two-way-binding-html-component';
+  fullName: string = "ankur israni";
+
+  constructor() {
+    setTimeout(() => {
+      this.fullName = "software developer";
+    }, 5000);
+  }
 }
